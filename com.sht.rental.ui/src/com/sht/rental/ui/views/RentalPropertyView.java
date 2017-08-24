66cca1a6e4dbbae6f2a9379b7b8ab420a1cefff6
gmorize <gmorize@LFR008150.corp.capgemini.com>
2017-08-24 17:38:26 +0200

    Vue rental améliorée

--- com.sht.rental.ui/src/com/sht/rental/ui/views/RentalPropertyView.java
+++ com.sht.rental.ui/src/com/sht/rental/ui/views/RentalPropertyView.java
@@ -16,6 +16,9 @@ public class RentalPropertyView extends ViewPart {
 	private Label rentedObjectLabel;
 	private Label customerName;
 	private Rental rentalObject;
+	private Label lblCustomername;
+	private Label lblDatedeb;
+	private Label lblDatefin;
 
 	public RentalPropertyView() {
 		rentalObject = null;
@@ -26,19 +29,49 @@ public class RentalPropertyView extends ViewPart {
 		parent.setLayout(new GridLayout(1, false));
 		
 		Group infoGroup = new Group(parent, SWT.NONE);
+		infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		infoGroup.setText("Informations");
+		infoGroup.setLayout(new GridLayout(2, false));
 		
 		rentedObjectLabel = new Label(infoGroup, SWT.NONE);
+		rentedObjectLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 		GridData gd = new GridData();
 		gd.horizontalSpan = 2;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
+		//new Label(infoGroup, SWT.NONE);
+		
+		Label lblLou = new Label(infoGroup, SWT.NONE);
+		lblLou.setText("Lou\u00E9 \u00E0");
+		
+		lblCustomername = new Label(infoGroup, SWT.NONE);
+		lblCustomername.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Group grpDates = new Group(parent, SWT.NONE);
+		grpDates.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		grpDates.setText("Dates");
+		grpDates.setLayout(new GridLayout(2, false));
+		
+		Label lblDd = new Label(grpDates, SWT.NONE);
+		lblDd.setText("du:");
+		
+		lblDatedeb = new Label(grpDates, SWT.NONE);
+		lblDatedeb.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		lblDatedeb.setText("");
+		
+		Label lblAu = new Label(grpDates, SWT.NONE);
+		lblAu.setText("au:");
+		
+		lblDatefin = new Label(grpDates, SWT.NONE);
 		
 		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
 	}
 
 	private void setRental(Rental r) {
 		rentedObjectLabel.setText(r.getRentedObject().getName());
+		lblCustomername.setText(r.getCustomer().getDisplayName());
+		lblDatedeb.setText(r.getStartDate().toString());
+		lblDatefin.setText(r.getEndDate().toString());
 	}
 
 	@Override

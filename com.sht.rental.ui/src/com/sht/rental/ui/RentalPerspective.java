5ea0c8c29bc17d6210cec8a443ea4b5d5d7a8cb6
gmorize <gmorize@LFR008150.corp.capgemini.com>
2017-08-25 16:33:15 +0200

    Perspective

---
+++ com.sht.rental.ui/src/com/sht/rental/ui/RentalPerspective.java
@@ -0,0 +1,20 @@
+package com.sht.rental.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		String editorArea = layout.getEditorArea();
+		
+		layout.addView("com.sht.rental.ui.views.AgencyView", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("com.sht.rental.ui.RentalPropertyView", IPageLayout.RIGHT, 0.5f, "com.sht.rental.ui.views.AgencyView");
+		}
+
+
+}
